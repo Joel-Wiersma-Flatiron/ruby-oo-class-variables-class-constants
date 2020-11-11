e7891a738be455784caf926146884268e1915d5f
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-10 19:06:40 -0700

    commit tree c5e98b1bde3c53761ae01c36212342c545ed0313

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    skip = false
+    BRANDS.each do |bran|
+      skip = true if bran == brand
+    end
+    BRANDS << brand if skip == false
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
